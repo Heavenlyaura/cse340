7a709e7328de7584cf0e683eb7c1bff95264c924
heavenlyaura <goodnessokafor462@gmail.com>
2024-04-20 18:14:04 +0100

    first commit

--- server.js
+++ server.js
@@ -7,13 +7,27 @@
  *************************/
 const express = require("express")
 const env = require("dotenv").config()
+const expressLayouts = require("express-ejs-layouts")
 const app = express()
 const static = require("./routes/static")
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+// Index Route
+app.get('/', (req, res) => {
+  res.render('index', {title: 'Home'})
+})
 
 /* ***********************
  * Local Server Information
