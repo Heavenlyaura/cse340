5c9c68e8136a1e9b63f0bd7bd7e2ba9633a6efb0
heavenlyaura <goodnessokafor462@gmail.com>
2024-04-29 00:41:15 +0100

    " W04 - Added express-validator for account registration data validation"
    This diff adds the "express-validator" package to the project dependencies and uses it to validate the registration data in the account route. The registration form in the "register.ejs

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = new express.Router()
 const utilities = require('../utilities')
 const accountController = require('../controllers/accountController')
+const regValidate = require('../utilities/account-validation')
+
 
 /* Get the Login in view for the Application */
 router.get('/login', utilities.handleErrors(accountController.buildLogin))
@@ -10,6 +12,12 @@ router.get('/login', utilities.handleErrors(accountController.buildLogin))
 router.get('/register', utilities.handleErrors(accountController.buildRegister))
 
 /* Procesess the registration process */
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+// Process the registration data
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
 
 module.exports = router
