b2af82ae21b640759609ecb8c76da6cf4d4070fe
heavenlyaura <goodnessokafor462@gmail.com>
2024-05-02 18:14:22 +0100

    Add JWT authentication to account management
    - Add jsonwebtoken and cookie-parser packages
    - Create endpoints for user login and account view
    - Implement JWT-based authentication using jsonwebtoken and cookie-parser
    -

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const router = new express.Router()
 const utilities = require('../utilities')
 const accountController = require('../controllers/accountController')
 const regValidate = require('../utilities/account-validation')
+const { route } = require('./static')
 
 
 /* Get the Login in view for the Application */
@@ -10,6 +11,10 @@ router.get('/login', utilities.handleErrors(accountController.buildLogin))
 
 /* Gets the Registration view for the Application */
 router.get('/register', utilities.handleErrors(accountController.buildRegister))
+
+/* Gets the Account view for the Application */
+router.get('/', utilities.handleErrors(accountController.getAccountView))
+
 
 /* Procesess the registration process */
 // Process the registration data
@@ -25,9 +30,7 @@ router.post(
   "/login",
   regValidate.loginRules(),
   regValidate.checkLoginData,
-  (req, res) => {
-    res.status(200).send('login process')
-  }
+  utilities.handleErrors(accountController.accountLogin)
 )
 
 module.exports = router
